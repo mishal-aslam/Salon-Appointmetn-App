7b854e746431f12699d7e1c44e69b12b05456015
Mishal Aslam <mishalaslam17@gmail.com>
2024-06-24 23:25:41 +0500

    header

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,52 @@
+// components/Button.tsx
+import React, { ReactNode } from "react";
+
+interface ButtonProps {
+  type?: "primary" | "secondary";
+  children: ReactNode;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type = "primary",
+  children,
+  onClick,
+  className,
+  disabled,
+}) => {
+  const baseClasses = "px-5 py-3 rounded font-semibold focus:outline-none";
+  const typeClasses = {
+    primary:
+      "bg-primary text-secondary border border-secondary hover:bg-danger rounded-full ",
+    secondary: "bg-secondary text-primary hover:bg-secondaryDark rounded-full ",
+  };
+
+  const combinedClasses = `${baseClasses} ${typeClasses[type]} ${className}`;
+
+  return (
+    <button
+      className={combinedClasses}
+      //   onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  onClick: () => {},
+  className: "",
+  disabled: false,
+};
+
+export default Button;
+
+
+{/* <Button type="primary" onClick={() => alert('Primary Button Clicked')}>
+      Primary Button
+    </Button>
+    <Button type="secondary" onClick={() => alert('Secondary Button Clicked')} className="mt-2">
+      Secondary Button
+    </Button> */}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,92 @@
+// "use client"
+import React, { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import Button from "@/app/components/Button";
+
+const Header = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <header>
+      <nav className="border border-secondary h-[100px]  px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl my-auto mt-4">
+          <a
+            href="/"
+            className="flex items-center text-Secondary text-3xl font-semibold"
+          >
+            Glow Scout
+          </a>
+          <div className="flex items-center lg:order-2">
+            <Button
+              type="secondary"
+              onClick={() => alert("Primary Button Clicked")}
+            >
+              Sign In/Up
+            </Button>
+            <button
+              onClick={toggleMobileMenu}
+              className="inline-flex items-center p-2 ml-1 text-sm text-secondary rounded-lg lg:hidden  focus:outline-none "
+              aria-controls="mobile-menu-2"
+              aria-expanded={isMobileMenuOpen}
+            >
+              <span className="sr-only">Toggle menu</span>
+              {isMobileMenuOpen ? (
+                <FiX className="w-6 h-6 text-secondary" />
+              ) : (
+                <FiMenu className="w-6 h-6 text-secondary" />
+              )}
+            </button>
+          </div>
+          <div
+            className={`flex justify-between items-center w-full lg:flex lg:w-auto lg:order-1 ${
+              isMobileMenuOpen ? "block" : "hidden"
+            }`}
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-12  font-medium lg:flex-row lg:space-x-20 lg:mt-0">
+              <li>
+                <a
+                  href="/"
+                  className="block py-2 pr-4 pl-3 text-secondary text-xl rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 "
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/components/pages/Treatments"
+                  className="block py-2 pr-4 pl-3 text-secondary text-xl rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
+                >
+                  Treatments
+                </a>
+              </li>
+              <li>
+                <a
+                    href="/components/pages/Spas"
+                  className="block py-2 pr-4 pl-3 text-secondary text-xl rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 "
+                >
+                  Spas
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/components/pages/Blog"
+                  className="block py-2 pr-4 pl-3 text-secondary text-xl rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 "
+                >
+                  Blogs
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
